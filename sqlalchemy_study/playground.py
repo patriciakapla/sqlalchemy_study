dcbee89fb02f1c57f3baebeaaa3c6fa64952474b
Patricia Kapla <patriciakapla@gmail.com>
2026-06-12 07:17:27 -0300

    fix: rename files, implement .env

--- sqlalchemy_study/playground.py
+++ sqlalchemy_study/playground.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
-from os import environ
+from os import getenv
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+DATABASE_URL = str(getenv('DATABASE_URL'))
 
 # engine factory
 engine = create_engine(
-    environ['DATABASE_URL'],
-    #echo= True
-    )
+    DATABASE_URL,
+    # echo= True
+)
 
 print(f'engine \n{engine}\n')
 
@@ -15,14 +19,15 @@ print(f'engine pool \n{engine.pool}\n')
 # high cost processes
 
 
-con1 = engine.connect()      # connection to the DB
-con2 = engine.connect()      # connection to the DB
- 
+con1 = engine.connect()  # connection to the DB
+print('passowww')
+con2 = engine.connect()  # connection to the DB
+
 print(f'engine.pool.status \n{engine.pool.status()}\n')
 # prints pool status, with all the connections in memory
 
 
-print(f'connection \n{con1.connection.dbapi_connection}\n')  
+print(f'connection \n{con1.connection.dbapi_connection}\n')
 # prints the postgresql connector, no abstractions!
 
-con1.close()     # closes the connection
+con1.close()  # closes the connection
